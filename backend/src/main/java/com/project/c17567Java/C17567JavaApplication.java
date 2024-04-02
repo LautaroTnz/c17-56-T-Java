53ce2b9e8a22c2fc89634401e93ee3cf08c92ba7
lautaroTintez07 <lautarotintez@gmail.com>
2024-04-02 12:57:26 -0300

    version inicial back

---
+++ backend/src/main/java/com/project/c17567Java/C17567JavaApplication.java
@@ -0,0 +1,13 @@
+package com.project.c17567Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C17567JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C17567JavaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/c17567Java/C17567JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.c17567Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C17567JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
